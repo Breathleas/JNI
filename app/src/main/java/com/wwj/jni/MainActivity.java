6a8372d156a10f522934e326a3a988d708fd93c7
githubwwj <githubwwj@outlook.com>
2018-07-04 22:01:12 +0800

    新增JAVA与Native的基本数据类型

--- app/src/main/java/com/wwj/jni/MainActivity.java
+++ app/src/main/java/com/wwj/jni/MainActivity.java
@@ -21,8 +21,42 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
 
-        tv.setText(stringFromJNI()+"-----"+add(5,13));
+//        tv.setText(stringFromJNI()+"-----"+add(5,13));
+
+
+        StringBuilder stringBuilder=new StringBuilder();
+
+
+        int result=add(23,35);//58
+        stringBuilder.append("add(23,35)="+result+"-----");
+
+        short s1=923,s2=789;//134
+        short sresult=sub(s1,s2);
+        stringBuilder.append("sub(923,789)="+sresult+"\r\n");
+
+        double dres=multi(30,20.2); //606.6
+        stringBuilder.append("multi(30,20.22)="+dres+"----");
+
+        long lres=divi(180,30); //6
+        stringBuilder.append("divi(180,30)="+lres+"\r\n");
+
+        char ch=getChar('A');
+        stringBuilder.append("etChar('A')="+ch+"-----");
+
+        byte bres=sqrt((byte) 25);
+        stringBuilder.append("sqrt(25)="+bres+"\r\n");
+
+        float fres=power(2,5);
+        stringBuilder.append("power(2,5)="+fres+"-----");
+
+        boolean bool=isTrue(true);
+        stringBuilder.append("isTrue(true)="+bool+"\r\n");
+
+        tv.setText(stringBuilder.toString());
+
     }
+
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -30,7 +64,27 @@ public class MainActivity extends AppCompatActivity {
      */
     public native String stringFromJNI(); //声明一个原声函数
 
-    public native int add(int a,int b);
+    public native int add(int a, int b);
+    public native short sub(short a,short b);
+    public native double multi(double a,double b);
+    public native long divi(long a,long b);
+    public native char getChar(char a);
+    public native byte sqrt(byte a);
+    public native float power(float num,float b);
+    public native boolean isTrue(boolean b);
+
+
+    // byte类型字节数=1
+// boolean类型字节数=1
+// char类型字节数=2
+// short类型字节数=2
+// int类型字节数=4
+// long类型字节数=8
+// float类型字节数=4
+// double类型字节数=8
+
+
+
 
 
 }
--- app/src/main/jni/good.c
+++ app/src/main/jni/good.c
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <android/log.h>
+#include <math.h>
 
 #define LOGTAG "hello"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOGTAG,__VA_ARGS__)
@@ -9,14 +10,52 @@
 jint Java_com_wwj_jni_MainActivity_add(JNIEnv *env, jobject instance, jint a, jint b) {
 
 //    int __android_log_print(int prio, const char* tag, const char* fmt, ...)
-    char *str="hello log";
+    char *str = "hello log";
 //    __android_log_print(ANDROID_LOG_INFO,LOGTAG,"a=%d",a);
 //    __android_log_print(ANDROID_LOG_WARN,LOGTAG,"str=%s",str);
 //    __android_log_print(ANDROID_LOG_ERROR,LOGTAG,"b=%d",b);
 //    __android_log_print(ANDROID_LOG_WARN,LOGTAG,"a+b=%d",a+b);
-    LOGI("a=%d",a);
-    LOGW("my str=%s",str);
-    LOGE("b=%d",b);
-    LOGW("a+b=%d",a+b);
-    return a+b;
+    LOGI("a=%d", a);
+    LOGW("my str=%s", str);
+    LOGE("b=%d", b);
+    LOGW("a+b=%d", a + b);
+    return a + b;
+}
+
+jshort Java_com_wwj_jni_MainActivity_sub(JNIEnv *env, jobject instance, jshort a, jshort b) {
+    return a - b;
+}
+
+jdouble Java_com_wwj_jni_MainActivity_multi(JNIEnv *env, jobject instance, jdouble a, jdouble b) {
+    return a * b;
+}
+
+jlong Java_com_wwj_jni_MainActivity_divi(JNIEnv *env, jobject instance, jlong a, jlong b) {
+    return a / b;
+}
+
+jchar Java_com_wwj_jni_MainActivity_getChar(JNIEnv *env, jobject instance, jchar a) {
+    //A
+    return a = a + 3; //D
+}
+
+jbyte Java_com_wwj_jni_MainActivity_sqrt(JNIEnv *env, jobject instance, jbyte a) {
+    return sqrt(a);
+}
+
+jfloat Java_com_wwj_jni_MainActivity_power(JNIEnv *env, jobject instance, jfloat num, jfloat b) {
+    jfloat temp=num;
+    for (int i = 1; i < b; i++) {
+        num *= temp;
+    }
+    return num;
+
+}
+
+jboolean Java_com_wwj_jni_MainActivity_isTrue(JNIEnv *env, jobject instance, jboolean b) {
+    if(b){
+        return 1;
+    }else{
+        return 0;
+    }
 }
